337585e1757c0baf0e214cada2c96cfcc88c573b
Daniel Brenes <danbg31@gmail.com>
2020-03-11 15:35:17 -0600

    Comprobando sincronización

--- src/Inicio/Main.java
+++ src/Inicio/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main (String[] args){
 
-        System.out.println(Calculadora.Multiplicar(1, 5));
+        System.out.println("Hola mundo");
 
     }
 
